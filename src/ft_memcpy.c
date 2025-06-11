4bd2e5ac5e934c45f69becee665fd3ec7162628b
Jingyuan Zhang <justyuan.z@gmail.com>
2025-06-11 18:53:06 +0300

    memmove

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -1,6 +1,6 @@
 #include "vec.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 	unsigned char	*dest;
---
+++ src/ft_memmove.c
@@ -0,0 +1,20 @@
+#include "vec.h"
+
+void *memmove(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char	*dest;
+	const void	*source;
+
+	dest = (unsigned char *)dst;
+	source = (const unsigned char *)src;
+	i =  0;
+	if (dest <= source)
+	{
+		while()
+	}
+	else
+	{
+
+	}
+}
